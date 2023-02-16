3b8cdcf7d314298616fb663ed115ab8371069b64
Maksym Meshkov <smarty.maks13@gmail.com>
2023-02-16 14:13:29 +0100

    commit

--- CompaniesHouseParsing.EmailSending/EmailMessageBuilder/EmailMessageBuilder.cs
+++ CompaniesHouseParsing.EmailSending/EmailMessageBuilder/EmailMessageBuilder.cs
@@ -1,14 +1,31 @@
-﻿namespace CompaniesHouseParsing.EmailSending
+﻿using System.Net;
+
+namespace CompaniesHouseParsing.EmailSending
 {
     public class EmailMessageBuilder : IEmailMessageBuilder
     {
-        public IEmailMessageBuilder WithText(string text)
-        {
-
-        }
         public IEmailMessage Build()
         {
+            return new EmailMessage()
+            {
+                Sender = "krotkrotowskij@gmail.com",
+                Recipient = "smarty.maks13#gmail.com",
+                Subject = "Test",
+                Text = "Test text"
+            };
+        }
 
-        } 
+        public IEmailMessageBuilder WithText(string text)
+        {
+            var message = Build();
+            var client = new EmailSmtpClientFactory();
+            var createClient = client.Create("smtp.gmail.com", 587, new NetworkCredential { Password = "cwztcchhlltrzskg" }, true);
+            var sendMess = createClient.Send(message);
+        }
+
+        public IEmailMessageBuilder ToRcepient(string recepientEmailAddress)
+        {
+            
+        }
     }
 }
