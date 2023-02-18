532543a930e026d7ea62508f1f0e7887d1f12ec0
Maksym Meshkov <smarty.maks13@gmail.com>
2023-02-18 14:05:56 +0100

    status 200!

--- CompaniesHouseParser/Program.cs
+++ CompaniesHouseParser/Program.cs
@@ -21,7 +21,7 @@ class Program
         list.Add("00000B");
         list.Add("00000C");
         list.Add("00000D");
-        list.Add("00000E");
+        list.Add("00000E"); 
         list.Add("00000F");
         list.Add("00000G");
 
