bb4975138824ff5adad23dd85c5438034b36f7b0
Tol-kovuy <69402030+Tol-kovuy@users.noreply.github.com>
2023-03-23 19:10:26 +0100

    Delete CompaniesHouseParser.Logging directory

--- CompaniesHouseParser.Logging/ILogging.cs
+++
@@ -1,9 +0,0 @@
-﻿using CompaniesHouseParser.IoC;
-using Microsoft.Extensions.Logging;
-
-namespace CompaniesHouseParser.Logging;
-
-public interface ILogging : ISingletonDependency 
-{
-    void GetLogger<T>(LogLevel logLevel, string message);
-}
